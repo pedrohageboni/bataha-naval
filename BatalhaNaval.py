b9abcf2d0aecb81f0dc6dfc256bbc793af2bce88
Victor Maia <victorszmaia@gmail.com>
2025-05-29 18:10:14 -0300

    troso vasil

--- BatalhaNaval.py
+++ BatalhaNaval.py
@@ -1,4 +1,38 @@
+import random
 '''
 Objetivo:
 Desenvolver um jogo completo e funcional de Batalha Naval em Python, aplicando conceitos fundamentais de programação, como manipulação de matrizes bidimensionais, funções, estruturas de repetição, condicionais, validação de entrada, encapsulamento e exibição formatada no terminal.
 '''
+def menu_inicial():
+    ''' 
+    — para escolher modo e tamanho.
+    Parâmetros: None
+    Retorna:  
+    tamanho - tamanho do tabuleiro, por ex. 10 para matriz 10 x 10
+    modo: modo de jogo, (1) jogador vs jogador ou (2) jogador vs IA 
+    '''
+
+def configurar_tabuleiro():
+     '''
+     — para criar matrizes.
+     '''
+def posicionar_navios():
+     '''
+     — para permitir o posicionamento manual.
+'''
+def mostrar_tabuleiro():
+     '''
+     — para imprimir as matrizes formatadas.
+'''
+def realizar_ataque():
+     '''
+     — para processar os ataques e atualizar matrizes.
+'''
+def verificar_vitoria():
+     '''
+     — para checar se um jogador perdeu todos os navios.
+'''
+def delay():
+     '''
+     — implementar a pausa entre turnos inimigos.
+     '''
