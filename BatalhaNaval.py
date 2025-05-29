69c9ee96c9542f8ba858847ab87796a89d230aae
Victor Maia <victorszmaia@gmail.com>
2025-05-29 17:33:40 -0300

    inicio do projeto

---
+++ BatalhaNaval.py
@@ -0,0 +1,4 @@
+'''
+Objetivo:
+Desenvolver um jogo completo e funcional de Batalha Naval em Python, aplicando conceitos fundamentais de programação, como manipulação de matrizes bidimensionais, funções, estruturas de repetição, condicionais, validação de entrada, encapsulamento e exibição formatada no terminal.
+'''
